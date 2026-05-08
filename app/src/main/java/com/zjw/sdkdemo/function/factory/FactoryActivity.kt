b3079b34853248287b8b16bcc0dd87e04ed657d0
dengdongqi <1396141012@qq.com>
2026-05-08 10:18:12 +0800

    提交2.3.4

--- app/src/main/java/com/zjw/sdkdemo/function/factory/FactoryActivity.kt
+++ app/src/main/java/com/zjw/sdkdemo/function/factory/FactoryActivity.kt
@@ -14,7 +14,15 @@ class FactoryActivity : BaseActivity() {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
         setTitle(R.string.ch_factory)
-        initLogSet(tag, binding.layoutLog.llLog, binding.layoutLog.cxLog, binding.layoutLog.llLogContent, binding.layoutLog.btnClear, binding.layoutLog.btnSet,binding.layoutLog.btnSendLog)
+        initLogSet(
+            tag,
+            binding.layoutLog.llLog,
+            binding.layoutLog.cxLog,
+            binding.layoutLog.llLogContent,
+            binding.layoutLog.btnClear,
+            binding.layoutLog.btnSet,
+            binding.layoutLog.btnSendLog
+        )
         initListener()
     }
 
@@ -26,6 +34,14 @@ class FactoryActivity : BaseActivity() {
         clickCheckConnect(binding.btReset) {
             addLogI("btReset")
             ControlBleTools.getInstance().resetByProduction()
+        }
+        clickCheckConnect(binding.btSoftReset) {
+            addLogI("btSoftReset")
+            ControlBleTools.getInstance().softResetByProduction()
+        }
+        clickCheckConnect(binding.btHardReset) {
+            addLogI("btHardReset")
+            ControlBleTools.getInstance().hardResetByProduction()
         }
         clickCheckConnect(binding.btHeartLightLeakTest) {
             addLogI("btHeartLightLeakTest")
